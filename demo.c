09dcdf3e24aefa6c9962ecc52b171a5eb3f5b040
rahul11nishad <rahul004700@gmail.com>
2023-03-18 23:07:23 +0530

    this is shilly

--- demo.c
+++ demo.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+void pat(int n)
+{
+    for(int i=0;i<=n;i++)
+    {
+        printf('*');
+        printf("\n");
+    }
+}
 int main()
 {
     printf("hello git ....");
+    pat(10);
     return 0;
 }
